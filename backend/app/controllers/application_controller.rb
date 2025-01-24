2ae8274eecef72957e735baecbdacf7fbd444761
belone0 <belone00@gmail.com>
2025-01-24 15:24:41 -0300

    test render backend deploy

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  include Authentication
   include ActionController::Helpers
   include ActionController::Cookies
+  include Authentication
 end
