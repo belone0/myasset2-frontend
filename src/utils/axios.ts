8014a04a068cfe6bbfb347a3345209496339cced
belone0 <belone00@gmail.com>
2025-01-24 19:45:08 -0300

    asdas

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     // baseURL: "http://localhost:3000",
-    baseURL: "https://mysite-ke2r.onrender.com",
+    baseURL: "https://myasset2-backend.onrender.com",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
