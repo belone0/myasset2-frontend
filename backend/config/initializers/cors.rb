ce75ffc4628173d48067307f1fd1278c77645872
belone0 <belone00@gmail.com>
2025-01-24 10:31:48 -0300

    fix: adicionar origem para o novo ambiente de produção no CORS

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173" # Update with your Vue app's URL
+    origins "http://localhost:5173", "https://myasset2.vercel.app" # Update with your Vue app's URL
     resource "*",
              headers: :any,
              methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
