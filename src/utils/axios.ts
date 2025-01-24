9b651456d6c9e2602fc0a49b57a876070ac9f7c4
belone0 <belone00@gmail.com>
2025-01-24 16:44:12 -0300

    prod URL

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://mysite-ke2r.onrender.com",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
